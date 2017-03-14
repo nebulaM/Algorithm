760cdc2f05e2fddf687d5a57a004e780a9c9f681
nebulam <nebulam12@gmail.com>
2017-03-13 22:10:56 -0700

    leetcode add first missing positive

---
+++ leetcode/41_firstMissingPositive.cc
@@ -0,0 +1,22 @@
+/*
+ * Given [1,2,0] return 3,
+ * and [3,4,-1,1] return 2.
+ */
+class Solution {
+public:
+	//https://discuss.leetcode.com/topic/8293/my-short-c-solution-o-1-space-and-o-n-time/18
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+			//let nums[i-1]=i
+            while(nums[i]>0 && nums[i]<=nums.size() && nums[nums[i]-1]!=nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return nums.size()+1;
+    }
+};
