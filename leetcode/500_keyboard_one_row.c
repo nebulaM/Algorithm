69ddeb86ca1f8569a9324cf8811753f8a1de0d22
nebulam <nebulam12@gmail.com>
2017-04-20 22:35:21 -0700

    leetcode 500: use predefined size to bound char array in c, otherwise there is a bug: in gdb, contents of keyoard_layout[row] on line 19 looks correct but it becomes longer than expected after the pointer is passed to function from_one_row()

--- leetcode/500_keyboard_one_row.c
+++ leetcode/500_keyboard_one_row.c
@@ -2,68 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-int from_one_row(char* word, const char* keyboard_row);
+#define KEYBOARD_ROW_SIZE 10
+int from_one_row(char* word, const char keyboard_row[KEYBOARD_ROW_SIZE]);
 char** findWords(char** words, int wordsSize, int* returnSize);
-static const char keyoard_layout[10][10]={{'q','w','e','r','t','y','u','i','o','p'},{'a','s','d','f','g','h','j','k','l'},
-    {'z','x','c','v','b','n','m'}
+static const char keyoard_layout[3][KEYBOARD_ROW_SIZE]={{'q','w','e','r','t','y','u','i','o','p'},{'a','s','d','f','g','h','j','k','l','\0'},
+    {'z','x','c','v','b','n','m','\0'}
 };
 char** findWords(char** words, int wordsSize, int* returnSize) {
     char** words_from_one_row=malloc(sizeof(char*)*0);
     for(int i=0;i<wordsSize;++i){
+		bool found=false;
         for(int row=0;row<3;++row){
             int letter_index_in_row=0;
-            while(keyoard_layout[row][letter_index_in_row]!='\0'){
-                if(words[i][0]==keyoard_layout[row][letter_index_in_row] || 
-                words[i][0]==(keyoard_layout[row][letter_index_in_row]+32)){
+            while(letter_index_in_row<KEYBOARD_ROW_SIZE&&keyoard_layout[row][letter_index_in_row]!='\0'){
+                if((words[i][0]==keyoard_layout[row][letter_index_in_row]) || (words[i][0]==(keyoard_layout[row][letter_index_in_row]-32))){
                     int word_size=from_one_row(words[i],keyoard_layout[row]);
                     if(word_size>0){
                         words_from_one_row=(char **)realloc(words_from_one_row,sizeof(char*)*((*returnSize)+1));
                         words_from_one_row[(*returnSize)]=(char*) malloc(sizeof(char)*word_size);
                         strncpy(words_from_one_row[(*returnSize)++],words[i],word_size);
+						found=true;
+                        break;
                     }
                 }
-                printf("row is %d\n",row);
-                printf("letter_index_in_row is %d\n",letter_index_in_row);
                 letter_index_in_row++;
+            }
+			if(found){
+                break;
             }
         }
     }
     return words_from_one_row;
 }
 
-int from_one_row(char* word, const char* keyboard_row){
+int from_one_row(char* word, const char keyboard_row[KEYBOARD_ROW_SIZE]){
     if((*word)=='\0'){
         return -1;
     }
     int word_index=0;
-    while(word[word_index++]!='\0'){
+    while(word[word_index]!='\0'){
         int keyboard_row_index=0;
         bool found=false;
-        while(keyboard_row[keyboard_row_index++]!=0){
+        while(keyboard_row_index<KEYBOARD_ROW_SIZE&&keyboard_row[keyboard_row_index]!=0){
             if(word[word_index]==keyboard_row[keyboard_row_index] || 
-            word[word_index]==(keyboard_row[keyboard_row_index]+32)){
+            word[word_index]==(keyboard_row[keyboard_row_index]-32)){
+				//printf("word[%d] is %c, matches %c in keyboard [%d]\n",word_index,word[word_index],keyboard_row[keyboard_row_index],keyboard_row_index);
                 found=true;
                 break;
             }
+			keyboard_row_index++;
         }
         if(!found){
             return -1;
         }
+		word_index++;
     }
-    printf("index is %d\n",word_index);
-    return ++word_index;
+	/*printf("[");
+	int i=0;
+	while(i<word_index){
+		printf("%c",word[i]);
+		i++;
+	}
+	printf("]\n");
+    printf("index is %d\n",word_index);*/
+	
+    return word_index;
 }
 
 int main(void){
-	char ** test={"Hello","Alaska","Dad","Peace"};
+	char * test[10]={"asdfghjkla","qwertyuiopq","zxcvbnzzm"};
 	int returnSize=0;
 	char** words;
-	words=findWords(test,4,&returnSize);
+	words=findWords(test,3,&returnSize);
 	for(int i=0;i<returnSize;++i){
 		int j=0;
 		printf("[");
 		while(words[i][j]!='\0'){
-			printf("%s",words[i][j]);
+			printf("%c",words[i][j]);
 			j++;
 		}
 		printf("],");
